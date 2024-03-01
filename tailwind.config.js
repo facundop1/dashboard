b64b515f4570c18ba01e334905e449a20532c211
Agv01 <agusgomez82010@gmail.com>
2024-03-01 13:38:52 -0300

    add icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     extend: {
       backgroundImage: {
         "sideboard-pfp": "url('/src/assets/foto-perfil.jpg')",
+        "upboard-menu": "url('/src/assets/menu.png')",
+        "logo": "url('/src/assets/favicon.png')",
       },
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      }
     },
   },
   plugins: [],
