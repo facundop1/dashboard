f24193a34adac701edad3582c08e7fc415019882
Facundo Paez <facupv123@gmail.com>
2024-03-01 13:58:42 -0300

    .

--- src/components/Sideboard.jsx
+++ src/components/Sideboard.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 export default function Sideboard() {
     return (
-        <div>
-            <section class="flex">
+        <div class="bg-slate-500 mt-10">
+            <section class="flex p-4">
                 <div class="bg-sideboard-pfp w-14 h-14 bg-cover" />
-                <div>
-                    <p class="">Admin</p>
-                    <p class="text-green-400">● online</p>
+                <div class="pl-4">
+                    <p class="text-white">Admin</p>
+                    <p class="text-green">● online</p>
                 </div>
             </section>
-            <div class="">
-                <p>Dashboard</p>
+            <div class="bg-slate-800 p-4 shadow-md">
+                <p class="text-center text-white">Dashboard</p>
             </div>
 
-            <section class="">
-                <p class="">Crear nuevo post</p>
-                <p class="">Actualizar post</p>
-                <p class="">Eliminar post</p>
+            <section class="bg-slate-800 px-4 pb-4">
+                <p class="text-white">Crear nuevo post</p>
+                <p class="text-white">Actualizar post</p>
+                <p class="text-white">Eliminar post</p>
             </section>
 
         </div>
