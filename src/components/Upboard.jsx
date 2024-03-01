739ed72b47e17f45da9e77cf9edc238c87841f24
Agv01 <agusgomez82010@gmail.com>
2024-03-01 14:14:29 -0300

    m

--- src/components/Upboard.jsx
+++ src/components/Upboard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Upboard() {
     return (
-        <div class="flex items-center justify-between">
+        <div class="bg-blue flex justify-between items-center ">
 
             <div class="bg-upboard-menu w-10 h-10 bg-cover " />
             
             <div class="bg-logo w-10 h-10 bg-cover" /> 
-            <section class="flex">
+            <section class="flex items-center">
                 <div class="bg-sideboard-noti w-10 h-10 bg-cover" />
                 <div class="bg-sideboard-correo w-10 h-10 bg-cover" />  
                 <div class="bg-sideboard-pfp w-14 h-14 bg-cover" />
