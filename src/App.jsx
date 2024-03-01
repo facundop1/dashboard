73ba7f2759c2a9f550f31451a96372afd860e197
Agv01 <agusgomez82010@gmail.com>
2024-03-01 16:32:16 -0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Upboard />
-      <Sideboard />
+
     </div>
     
     
--- src/components/Upboard.jsx
+++ src/components/Upboard.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import Sideboard from './Sideboard'; 
 
 export default function Upboard() {
+    const [sideboardVisible, setSideboardVisible] = useState(false);
+
+    const toggleSideboard = () => {
+        setSideboardVisible(!sideboardVisible);
+    };
+
     return (
-        <div class="bg-gray-light    flex justify-between items-center p-[13px]">
-
-            <div class="bg-upboard-menu w-14 h-14 bg-cover " />
-            
-            <div class="bg-logo w-16 h-16 bg-cover" /> 
-        
-            <section class="flex items-center gap-[40px]">
-
-                <div class="flex">
-                <div class="bg-sideboard-noti w-14 h-14 bg-cover" />
-                <div class="bg-sideboard-correo w-14 h-14 bg-cover" />  
+        <div className="bg-gray-light flex justify-between items-center p-[13px]">
+            <div className="bg-upboard-menu w-14 h-14 bg-cover" onClick={toggleSideboard} />
+            <div className="bg-logo w-16 h-16 bg-cover" />
+            <section className="flex items-center gap-[40px]">
+                <div className="flex">
+                    <div className="bg-sideboard-noti w-14 h-14 bg-cover" />
+                    <div className="bg-sideboard-correo w-14 h-14 bg-cover" />
                 </div>
-
-                <div class= "flex items-center gap-[5px]">
-                <div class="bg-sideboard-pfp w-14 h-14 bg-cover " />
-                <p>Profile</p>
+                <div className="flex items-center gap-[5px]">
+                    <div className="bg-sideboard-pfp w-14 h-14 bg-cover" />
+                    <p>Profile</p>
                 </div>
-
             </section>
-           
-           
-           
-           
-           
-
+            {sideboardVisible && <Sideboard />}
         </div>
-    )
+    );
 }
