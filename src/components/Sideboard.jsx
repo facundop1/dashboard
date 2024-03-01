b076c0a0e6f703b32f7fa724e69d5b5f158b0300
Agv01 <agusgomez82010@gmail.com>
2024-03-01 16:42:29 -0300

    s

--- src/components/Sideboard.jsx
+++ src/components/Sideboard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Sideboard() {
     return (
-        <div class="bg-slate-500 mt-10">
-            <section class="flex p-4">
+        <div class="bg-slate-500 fixed h-full left-0 top-[90px] w-80 text-white">
+            <section class="flex p-4 ">
                 <div class="bg-sideboard-pfp w-14 h-14 bg-cover" />
                 <div class="pl-4">
                     <p class="text-white">Admin</p>
@@ -16,7 +16,7 @@ export default function Sideboard() {
                 <div class="border-t-[0.1rem] border-red-600"></div>
             </div>
 
-            <section class="bg-slate-800 px-4 pb-4 flex flex-col gap-2">
+            <section class="bg-slate-800 px-4 pb-4 flex flex-col gap-2 h-full">
                 <p class="text-white">Crear nuevo post</p>
                 <p class="text-white">Editar post</p>
                 <p class="text-white">Eliminar post</p>
