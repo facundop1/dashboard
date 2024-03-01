ca96aeb36f0e9fdd5b61c128ad33cfc38966d64f
Agv01 <agusgomez82010@gmail.com>
2024-03-01 13:24:31 -0300

    x

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import './input.css';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-    <Upboard />
-    <Sideboard />
-=======
+      <Upboard />
       <Sideboard />
->>>>>>> e65e6387a3d9c22f059f997d9f7416c1f96f1c1f
     </div>
     
     
