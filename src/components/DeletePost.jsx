bf0a0e8cb2b4046012f08b6793d7299dbeb2dfac
Agv01 <agusgomez82010@gmail.com>
2024-03-07 12:58:35 -0300

    x

--- src/components/DeletePost.jsx
+++ src/components/DeletePost.jsx
@@ -6,14 +6,17 @@ export default function DeletePost() {
             <section className="bg-slate-800 p-4 rounded">
                 <p className="text-white mb-2">Eliminar Post</p>
                 <div className="border-t-[0.1rem] border-red-600" />
-                <ul className="grid grid-cols-4 line text-white">
+                
+                <ul className="grid grid-cols-4  justify-center line text-white p-4 align-center ">
                     
                     <li>ID</li>
                     <li>Titulo</li>
                     <li>Fecha de carga</li>
-                    <li>Delete</li>
+                    <li className='bg-deletepost-trash w-4  h-4 bg-cover'></li>
                    
-                </ul>   
+                </ul> 
+
+                  
             </section>
         </div>
   )
